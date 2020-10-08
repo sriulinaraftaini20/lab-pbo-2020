e44a0b8b564ef729e3150addc2098aa1c134253b
Sri Ulina Raftaini <sriulinaraftaini20@gmail.com>
2020-10-09 03:27:13 +0700

    Membuat Kode Pertemuan 1

---
+++ Pertemuan 2/Array.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Array{
+	public static void main(String[] args){
+		//int items[] = {1,5,1,73,5};
+
+		/*for(int i=0; i<arr.length; i++){
+			System.out.println(arr[i]);
+
+		}*/
+		/*for(int item : items){ 
+			System.out.println(item); 
+		}*/
+		/*Scanner in = new Scanner(System.in);
+		int items[];
+
+		System.out.println("Masukkan Panjang array :");
+		int n =in.nextInt();
+
+		items = new int[n];
+
+		for(int i=0; i<items.length; i++){
+			System.out.println("Masukkan indeks ke-"+i+" :");
+			items[i] = in.nextInt();
+		}
+		for(int item: items){
+			System.out.println(item);
+		}*/
+		
+
+		//mencari jumlah semua arraynya
+		int items[] = {1,2,3,4,5};
+
+		for(int i = 0; i<items.length; i++){
+			System.out.println("Indeks ke-"+i+": " + items[i]);
+		}
+
+	}
+}
